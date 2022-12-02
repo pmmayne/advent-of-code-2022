d732a2b67d5fa931825b938742d6776c2494fcef
Pierre-Maël MAYNÉ <pierre-mael.mayne@dataiku.com>
2022-12-02 09:01:35 +0100

    Day 2 I am an idiot

--- kotlin/src/Day02.kt
+++ kotlin/src/Day02.kt
@@ -4,8 +4,79 @@ class Day02 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            readLinesFromResourceFile("day02")
+            val rounds =
+                    readLinesFromResourceFile("day02")
+                            .sumOf { it.toGame().toScore() }
+            println(rounds)
         }
     }
 
 }
+
+private fun String.toGame(): String {
+    return when (this) {
+        "A X" -> {
+            "A Z"
+        }
+        "A Y" -> {
+            "A X"
+        }
+        "A Z" -> {
+            "A Y"
+        }
+        "B X" -> {
+            "B X"
+        }
+        "B Y" -> {
+            "B Y"
+        }
+        "B Z" -> {
+            "B Z"
+        }
+        "C X" -> {
+            "C Y"
+        }
+        "C Y" -> {
+            "C Z"
+        }
+        "C Z" -> {
+            "C X"
+        }
+        else -> ""
+    }
+}
+
+private fun String.toScore(): Int {
+    return when (this) {
+        "A X" -> { // Rock vs Rock
+            3 + 1
+        }
+        "A Y" -> { // Rock vs Paper
+            6 + 2
+        }
+        "A Z" -> { // Rock vs Scissors
+            0 + 3
+        }
+        "B X" -> { // Paper vs Rock
+            0 + 1
+        }
+        "B Y" -> { // Paper vs Paper
+            3 + 2
+        }
+        "B Z" -> { // Paper vs Scissors
+            6 + 3
+        }
+        "C X" -> { // Scissors vs Rock
+            6 + 1
+        }
+        "C Y" -> { // Scissors vs Paper
+            0 + 2
+        }
+        "C Z" -> { // Scissors vs Scissors
+            3 + 3
+        }
+        else -> 0
+    }
+
+}
+
